d0d715a383dbfd25b7c6bce95e78d69e98944e53
Will Kennedy <willmlb@sbcglobal.net>
2020-09-10 03:39:37 -0400

    fixed help message

--- OsuMissAnalyzer.Server/Program.cs
+++ OsuMissAnalyzer.Server/Program.cs
@@ -28,7 +28,7 @@ namespace OsuMissAnalyzer.Server
         const string HELP_MESSAGE = @"osu! Miss Analyzer bot
 ```
 Usage:
-  >miss {{user-recent|user-top\}} <username> [<index>]
+  >miss {user-recent|user-top} <username> [<index>]
     Finds #index recent/top play for username (index defaults to 1)
   >miss beatmap <beatmap id/beatmap link> [<index>]
     Finds #index score on beatmap (index defaults to 1)
